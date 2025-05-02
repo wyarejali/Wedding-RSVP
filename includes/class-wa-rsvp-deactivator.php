2112499673a9fda329ba0621dd86b2c8523f0428
Wyarej Ali <wyarejali@Wyarejs-Mac-mini.local>
2025-05-02 10:43:18 +0600

    Fixed deactivate issue and readme file

--- includes/class-wa-rsvp-deactivator.php
+++ includes/class-wa-rsvp-deactivator.php
@@ -1,12 +1,12 @@
 <?php
 class WA_RSVP_Deactivator {
     public static function deactivate() {
-        global $wpdb;
+        // global $wpdb;
 
-        // Drop the RSVP submissions table
-        $table_name = $wpdb->prefix . 'wa_rsvp_submissions';
-        $sql        = "DROP TABLE IF EXISTS $table_name;";
-        $wpdb->query( $sql );
+        // // Drop the RSVP submissions table
+        // $table_name = $wpdb->prefix . 'wa_rsvp_submissions';
+        // $sql        = "DROP TABLE IF EXISTS $table_name;";
+        // $wpdb->query( $sql );
 
         // Delete the RSVP page
         // $rsvp_page = get_page_by_path( 'rsvp' );
